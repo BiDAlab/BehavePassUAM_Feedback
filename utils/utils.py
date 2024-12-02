cc849554103714db9c46587e664134b797eaedf5
martus-robledomoreno <marta.robledo@uam.es>
2024-12-02 15:32:19 +0100

    t

--- utils/utils.py
+++ utils/utils.py
@@ -30,7 +30,7 @@ def connect_ftps():
     ftps.encoding = "latin-1"
 
     # Conectar al servidor FTPS
-    ftps.connect(host, port, timeout = 30)
+    ftps.connect(host, port, timeout = 10t)
     ftps.login(username, password)
 
     # Forzar la protección de datos (TLS para el canal de datos)
