7d9ae37fc89107a2109e85dc71fb41696ee3f858
martus-robledomoreno <marta.robledo@uam.es>
2024-12-05 16:01:49 +0100

    a

--- utils/utils.py
+++ utils/utils.py
@@ -11,7 +11,7 @@ from Crypto.Cipher import AES
 from Crypto.Util.Padding import unpad
 import base64
 import time
-
+import requests
 
 from .enums import TestFileName
 
--- views/dashboard.py
+++ views/dashboard.py
@@ -7,6 +7,7 @@ from streamlit_extras.add_vertical_space import add_vertical_space
 from utils import *
 from utils.enums import TabsEnums
 from views.tabs import *
+import requests
 
 
 usuario_en = st.query_params.feedback
