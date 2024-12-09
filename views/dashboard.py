176a6649206f83b939900454a85bc23c825ed9bf
martus-robledomoreno <marta.robledo@uam.es>
2024-12-09 09:46:38 +0100

    e

--- views/dashboard.py
+++ views/dashboard.py
@@ -124,7 +124,7 @@ else:
 if st.session_state.selected_tab_id == TabsEnums.SUMMARY.value:
     render_summary_tab()
 elif st.session_state.selected_tab_id == TabsEnums.TAP.value:
-    render_tap_tab_json()
+    render_tap_tab_json(json_usuario, lang)
 elif st.session_state.selected_tab_id == TabsEnums.READ.value:
     st.warning(f"{json_usuario}")
     #render_tap_tab_json(json_usuario['response'], lang)
