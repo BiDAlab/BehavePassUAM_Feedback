1063442196da99cd7b4257b794fe1cbdd182be63
martus-robledomoreno <marta.robledo@uam.es>
2024-12-09 13:30:12 +0100

    a

--- views/tabs/__init__.py
+++ views/tabs/__init__.py
@@ -3,4 +3,4 @@ from .tap import render_tap_tab_json
 from .sign import render_sign_tab_json
 from .pattern import render_pattern_tab_json
 from .read_text import render_read_tab_json
-from .galeria import render_galeria_tab
+from .galeria import render_galeria_tab_json
